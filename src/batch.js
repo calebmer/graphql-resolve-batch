01ef96f16b6f274fd6d9f501df011dbf827e2d0d
Caleb Meredith <calebmeredith8@gmail.com>
2017-02-27 19:20:37 -0500

    initial commit

---
+++ src/batch.js
@@ -0,0 +1,175 @@
+/**
+ * Creates a GraphQL.js field resolver that batches together multiple resolves
+ * together that share the *exact* same GraphQL field selection.
+ */
+export function createBatchResolver(batchResolveFn) {
+  // Throw an error early of the batch resolver is not a function instead of
+  // throwing an error at query execution time.
+  if (typeof batchResolveFn !== 'function') {
+    throw new Error(
+      'Expected a function as the first argument when creating a batch ' +
+        `resolver. Instead found: '${typeof batchResolveFn}'.`,
+    );
+  }
+  const batcher = new Batcher(batchResolveFn);
+  return (source, args, context, info) => {
+    return new Promise((resolve, reject) => {
+      batcher.batch(source, args, context, info, resolve, reject);
+    });
+  };
+}
+
+/**
+ * A structure that batches GraphQL resolves together based on the GraphQL field
+ * nodes.
+ */
+class Batcher {
+  constructor(batchResolveFn) {
+    this._batchResolveFn = batchResolveFn;
+    this._batches = new Map();
+  }
+
+  /**
+   * Registers a batch execution for the provided GraphQL field node ASTs. The
+   * batch resolver function should be scheduled to execute on the next tick and
+   * the batch resolver finishes the `resolve` and `reject` functions will be
+   * executed.
+   *
+   * We group batches together by `info.fieldNodes`.
+   */
+  batch(source, args, context, info, resolve, reject) {
+    const { fieldNodes } = info;
+    let batch = this._batches.get(fieldNodes);
+
+    // If no batch currently exists for this array of field nodes then we want
+    // to create one.
+    if (typeof batch === 'undefined') {
+      batch = {
+        // We only use the first set of `args`, `context`, and `info` that we
+        // are passed.
+        //
+        // It is mostly safe to assume that these variables will be the same
+        // for the same `fieldNodes` from the execution implementation.
+        args,
+        context,
+        info,
+
+        // We will push our sources and promise callbacks into these arrays.
+        sources: [],
+        callbacks: [],
+      };
+      this._batches.set(fieldNodes, batch);
+    }
+
+    // Add our source and callbacks to the batch.
+    batch.sources.push(source);
+    batch.callbacks.push({ resolve, reject });
+
+    // Schedule a resolve if none has already been scheduled.
+    this._scheduleResolve();
+  }
+
+  /**
+   * Schedules a resolve for the next tick if a resolve has not already been
+   * scheduled.
+   */
+  _scheduleResolve() {
+    if (!this._hasScheduledResolve) {
+      this._hasScheduledResolve = true;
+      process.nextTick(() => {
+        this._resolve();
+        this._hasScheduledResolve = false;
+      });
+    }
+  }
+
+  /**
+   * Resolves all of the batch callbacks by actually executing the batch
+   * resolver.
+   */
+  _resolve() {
+    // Execute every batch that has accumulated.
+    this._batches.forEach(batch => {
+      // Execute our batch resolver function with the appropriate arguments. We
+      // use the `executePromise` function to normalize synchronous and
+      // asynchronous execution.
+      executePromise(
+        this._batchResolveFn,
+        batch.sources,
+        batch.args,
+        batch.context,
+        batch.info,
+      )
+        .then(
+          // If we got back an array of values then we want to resolve all of our
+          // callbacks for this batch.
+          values => {
+            // Throw an error if we did not get an array of values back from the
+            // batch resolver function.
+            if (!Array.isArray(values)) {
+              throw new Error(
+                'Must return an array of values from the batch resolver ' +
+                  'function. Instead the function returned a ' +
+                  `'${Object.prototype.toString.call(values)}'.`,
+              );
+            }
+            // Throw an error if the array of values we got back from the resolver
+            // is not equal to the number of values we expected when looking at
+            // the sources.
+            if (values.length !== batch.sources.length) {
+              throw new Error(
+                'Must return the same number of values from the batch ' +
+                  'resolver as there were sources. Expected ' +
+                  `${batch.sources.length} value(s) but got ` +
+                  `${values.length} value(s).`,
+              );
+            }
+            // We want to call all of our callbacks with a value returned by our
+            // batch resolver.
+            batch.callbacks.forEach(({ resolve, reject }, i) => {
+              // Get the value for this set of callbacks. If it is an error then
+              // we want to reject this promise. Otherwise we will resolve to the
+              // value.
+              const value = values[i];
+              if (value instanceof Error) {
+                reject(value);
+              } else {
+                resolve(value);
+              }
+            });
+          },
+        )
+        // If we got an error we want to reject all of our callbacks.
+        .catch(error => {
+          batch.callbacks.forEach(({ reject }) => {
+            reject(error);
+          });
+        });
+    });
+    // Clean out our batches map.
+    this._batches.clear();
+  }
+}
+
+/**
+ * Executes a function and *always* returns a function. If the function executes
+ * synchronously then the result will be coerced into a promise, and if the
+ * function returns a promise then that promise will be returned.
+ */
+function executePromise(fn, ...args) {
+  try {
+    // Execute the function. We do not bind a `this` variable. This is
+    // expected to be done by the caller.
+    const result = fn(...args);
+    // If the result is thenable (most likely a promise) then we want to return
+    // the result directly. Otherwise we will turn the value into a promise with
+    // `Promise.resolve`.
+    return result && typeof result.then === 'function'
+      ? result
+      : Promise.resolve(result);
+  } catch (error) {
+    // If the functioned errored synchronously we want to return a promise that
+    // immeadiately rejects.
+    return Promise.reject(error);
+  }
+}
---
+++ src/batch.test.js
@@ -0,0 +1,281 @@
+import { createBatchResolver } from './batch';
+
+describe('createBatchResolver', () => {
+  it('will throw an error if a function is not the first argument', () => {
+    expect(() => {
+      createBatchResolver();
+    }).toThrow(
+      'Expected a function as the first argument when creating a batch ' +
+        "resolver. Instead found: 'undefined'.",
+    );
+    expect(() => {
+      createBatchResolver('hello world');
+    }).toThrow(
+      'Expected a function as the first argument when creating a batch ' +
+        "resolver. Instead found: 'string'.",
+    );
+    expect(() => {
+      createBatchResolver(42);
+    }).toThrow(
+      'Expected a function as the first argument when creating a batch ' +
+        "resolver. Instead found: 'number'.",
+    );
+    expect(() => {
+      createBatchResolver({});
+    }).toThrow(
+      'Expected a function as the first argument when creating a batch ' +
+        "resolver. Instead found: 'object'.",
+    );
+    expect(() => {
+      createBatchResolver(null, () => {});
+    }).toThrow(
+      'Expected a function as the first argument when creating a batch ' +
+        "resolver. Instead found: 'object'.",
+    );
+  });
+
+  it('will batch basic synchronous resolves grouping with `fieldNodes`', () => {
+    const batchResolve = jest.fn(sources =>
+      sources.map(source => source + 0.5));
+
+    const resolve = createBatchResolver(batchResolve);
+
+    const fieldNodes1 = Symbol('fieldNodes');
+    const fieldNodes2 = Symbol('fieldNodes');
+
+    return Promise.resolve()
+      .then(() => {
+        return resolve(0, null, null, { fieldNodes: fieldNodes1 });
+      })
+      .then(value => {
+        expect(value).toEqual(0.5);
+        expect(batchResolve.mock.calls.length).toEqual(1);
+        return resolve(1, null, null, { fieldNodes: fieldNodes1 });
+      })
+      .then(value => {
+        expect(value).toEqual(1.5);
+        expect(batchResolve.mock.calls.length).toEqual(2);
+        return resolve(2, null, null, { fieldNodes: fieldNodes1 });
+      })
+      .then(value => {
+        expect(value).toEqual(2.5);
+        expect(batchResolve.mock.calls.length).toEqual(3);
+        return Promise.all([
+          resolve(3, null, null, { fieldNodes: fieldNodes1 }),
+          resolve(4, null, null, { fieldNodes: fieldNodes1 }),
+          resolve(5, null, null, { fieldNodes: fieldNodes1 }),
+        ]);
+      })
+      .then(values => {
+        expect(values).toEqual([3.5, 4.5, 5.5]);
+        expect(batchResolve.mock.calls.length).toEqual(4);
+        return Promise.all([
+          resolve(6, null, null, { fieldNodes: fieldNodes1 }),
+          resolve(7, null, null, { fieldNodes: fieldNodes2 }),
+          resolve(8, null, null, { fieldNodes: fieldNodes1 }),
+          resolve(9, null, null, { fieldNodes: fieldNodes1 }),
+        ]);
+      })
+      .then(values => {
+        expect(values).toEqual([6.5, 7.5, 8.5, 9.5]);
+        expect(batchResolve.mock.calls.length).toEqual(6);
+        expect(batchResolve.mock.calls).toEqual([
+          [[0], null, null, { fieldNodes: fieldNodes1 }],
+          [[1], null, null, { fieldNodes: fieldNodes1 }],
+          [[2], null, null, { fieldNodes: fieldNodes1 }],
+          [[3, 4, 5], null, null, { fieldNodes: fieldNodes1 }],
+          [[6, 8, 9], null, null, { fieldNodes: fieldNodes1 }],
+          [[7], null, null, { fieldNodes: fieldNodes2 }],
+        ]);
+      });
+  });
+
+  it('will pass the first `args` to the batch resolver', () => {
+    const batchResolve = jest.fn(sources =>
+      sources.map(source => source + 0.5));
+
+    const resolve = createBatchResolver(batchResolve);
+
+    const fieldNodes = Symbol('fieldNodes');
+    const args1 = Symbol('args1');
+    const args2 = Symbol('args2');
+    const args3 = Symbol('args3');
+
+    return Promise.resolve()
+      .then(() => {
+        return Promise.all([
+          resolve(0, args1, null, { fieldNodes }),
+          resolve(1, args2, null, { fieldNodes }),
+          resolve(2, args3, null, { fieldNodes }),
+        ]);
+      })
+      .then(values => {
+        expect(values).toEqual([0.5, 1.5, 2.5]);
+        expect(batchResolve.mock.calls).toEqual([
+          [[0, 1, 2], args1, null, { fieldNodes }],
+        ]);
+      });
+  });
+
+  it('will pass the first `context` to the batch resolver', () => {
+    const batchResolve = jest.fn(sources =>
+      sources.map(source => source + 0.5));
+
+    const resolve = createBatchResolver(batchResolve);
+
+    const fieldNodes = Symbol('fieldNodes');
+    const context1 = Symbol('context1');
+    const context2 = Symbol('context2');
+    const context3 = Symbol('context3');
+
+    return Promise.resolve()
+      .then(() => {
+        return Promise.all([
+          resolve(0, null, context1, { fieldNodes }),
+          resolve(1, null, context2, { fieldNodes }),
+          resolve(2, null, context3, { fieldNodes }),
+        ]);
+      })
+      .then(values => {
+        expect(values).toEqual([0.5, 1.5, 2.5]);
+        expect(batchResolve.mock.calls).toEqual([
+          [[0, 1, 2], null, context1, { fieldNodes }],
+        ]);
+      });
+  });
+
+  it(
+    'will pass the first `info` to the batch resolver even if `fieldNodes` ' +
+      'is the same',
+    () => {
+      const batchResolve = jest.fn(sources =>
+        sources.map(source => source + 0.5));
+
+      const resolve = createBatchResolver(batchResolve);
+
+      const fieldNodes = Symbol('fieldNodes');
+      const extra1 = Symbol('extra1');
+      const extra2 = Symbol('extra2');
+      const extra3 = Symbol('extra3');
+
+      return Promise.resolve()
+        .then(() => {
+          return Promise.all([
+            resolve(0, null, null, { fieldNodes, extra1 }),
+            resolve(1, null, null, { fieldNodes, extra2 }),
+            resolve(2, null, null, { fieldNodes, extra3 }),
+          ]);
+        })
+        .then(values => {
+          expect(values).toEqual([0.5, 1.5, 2.5]);
+          expect(batchResolve.mock.calls).toEqual([
+            [[0, 1, 2], null, null, { fieldNodes, extra1 }],
+          ]);
+        });
+    },
+  );
+
+  it('will reject if an array is not returned by the resolver', () => {
+    const resolve0 = createBatchResolver(() => null);
+    const resolve1 = createBatchResolver(() => 42);
+    const resolve2 = createBatchResolver(() => 'Hello, world!');
+    const resolve3 = createBatchResolver(() => ({}));
+
+    const fieldNodes = Symbol('fieldNodes');
+
+    const identity = value => value;
+    const unexpected = () => {
+      throw new Error('Unexpected.');
+    };
+
+    return Promise.all([
+        resolve0(null, null, null, { fieldNodes }).then(unexpected, identity),
+        resolve1(null, null, null, { fieldNodes }).then(unexpected, identity),
+        resolve2(null, null, null, { fieldNodes }).then(unexpected, identity),
+        resolve3(null, null, null, { fieldNodes }).then(unexpected, identity),
+      ])
+      .then(errors => {
+        expect(errors.map(({ message }) => message)).toEqual([
+          'Must return an array of values from the batch resolver ' +
+            "function. Instead the function returned a '[object Null]'.",
+          'Must return an array of values from the batch resolver ' +
+            "function. Instead the function returned a '[object Number]'.",
+          'Must return an array of values from the batch resolver ' +
+            "function. Instead the function returned a '[object String]'.",
+          'Must return an array of values from the batch resolver ' +
+            "function. Instead the function returned a '[object Object]'.",
+        ]);
+      });
+  });
+
+  it(
+    'will reject if the returned value does not have the same length as ' +
+      'the sources',
+    () => {
+      const resolve0 = createBatchResolver(() => []);
+      const resolve1 = createBatchResolver(() => [1]);
+      const resolve2 = createBatchResolver(() => [1, 2]);
+
+      const fieldNodes = Symbol('fieldNodes');
+
+      const identity = value => value;
+      const unexpected = () => {
+        throw new Error('Unexpected.');
+      };
+
+      return Promise.all([
+          resolve0(null, null, null, { fieldNodes }).then(unexpected, identity),
+          resolve1(null, null, null, { fieldNodes }).then(unexpected, identity),
+          resolve1(null, null, null, { fieldNodes }).then(unexpected, identity),
+          resolve1(null, null, null, { fieldNodes }).then(unexpected, identity),
+          resolve2(null, null, null, { fieldNodes }).then(unexpected, identity),
+        ])
+        .then(errors => {
+          expect(errors.map(({ message }) => message)).toEqual([
+            'Must return the same number of values from the batch resolver ' +
+              'as there were sources. Expected 1 value(s) but got 0 value(s).',
+            'Must return the same number of values from the batch resolver ' +
+              'as there were sources. Expected 3 value(s) but got 1 value(s).',
+            'Must return the same number of values from the batch resolver ' +
+              'as there were sources. Expected 3 value(s) but got 1 value(s).',
+            'Must return the same number of values from the batch resolver ' +
+              'as there were sources. Expected 3 value(s) but got 1 value(s).',
+            'Must return the same number of values from the batch resolver ' +
+              'as there were sources. Expected 1 value(s) but got 2 value(s).',
+          ]);
+          expect(errors[1]).not.toBe(errors[0]);
+          expect(errors[1]).toBe(errors[1]);
+          expect(errors[1]).toBe(errors[2]);
+          expect(errors[1]).toBe(errors[3]);
+          expect(errors[1]).not.toBe(errors[4]);
+        });
+    },
+  );
+
+  it('will reject individual promises if errors are returned', () => {
+    const error1 = new Error('Yikes 1');
+    const error2 = new Error('Yikes 1');
+
+    const resolve = createBatchResolver(() => [1, error1, 3, 4, error2]);
+
+    const fieldNodes = Symbol('fieldNodes');
+
+    const identity = value => value;
+    const unexpected = () => {
+      throw new Error('Unexpected.');
+    };
+
+    return Promise.all([
+        resolve(null, null, null, { fieldNodes }).then(identity, unexpected),
+        resolve(null, null, null, { fieldNodes }).then(unexpected, identity),
+        resolve(null, null, null, { fieldNodes }).then(identity, unexpected),
+        resolve(null, null, null, { fieldNodes }).then(identity, unexpected),
+        resolve(null, null, null, { fieldNodes }).then(unexpected, identity),
+      ])
+      .then(results => {
+        expect(results).toEqual([1, error1, 3, 4, error2]);
+        expect(results[1]).toEqual(error1);
+        expect(results[4]).toEqual(error2);
+      });
+  });
+});
